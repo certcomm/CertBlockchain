46cab42ccf146829928302bd3561d02e19a9e265
Kalpesh Patel <kpatel@tmailinc.com>
2018-10-30 08:49:37 -0500

    used gaanache for local instead of testrpc

--- ethereum/truffle.js
+++ ethereum/truffle.js
@@ -22,7 +22,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
     },
     rinkeby: {
